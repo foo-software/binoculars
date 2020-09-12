955610d12fde4fddb72044fe5ed5abbe494f06d8
adamhenson <ahenson@paperlesspost.com>
2020-09-11 21:13:19 -0400

    refactor: config generation

---
+++ src/bin/generateAuditDefinitions.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+// import fs from 'fs';
+// import path from 'path';
+
+// const auditConfigPath = path.resolve('./dist/config/audits');
+const run = () => console.log('generate audit defs');
+export default run;
