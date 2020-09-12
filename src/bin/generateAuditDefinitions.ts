b708d94efc614915a14ac377d346c429455f9be1
Adam Henson <adamhenson1979@gmail.com>
2020-09-12 15:43:50 -0400

    feat: generateAuditDefinitions

--- src/bin/generateAuditDefinitions.ts
+++ src/bin/generateAuditDefinitions.ts
@@ -1,7 +1,23 @@
 #!/usr/bin/env node
 // import fs from 'fs';
 // import path from 'path';
+import getDocumentationLinks from '../helpers/getDocumentationLinks';
+import * as audits from '../config/audits';
 
-// const auditConfigPath = path.resolve('./dist/config/audits');
-const run = () => console.log('generate audit defs');
-export default run;
+interface Audit {
+  [key: string]: any;
+}
+
+const auditColletion: Audit = audits;
+const links = [];
+
+for (const key in auditColletion) {
+  const { description } = auditColletion[key]();
+  const descriptionLinks = getDocumentationLinks(description);
+
+  if (descriptionLinks.length) {
+    links.push(...descriptionLinks);
+  }
+}
+
+console.log('links', links);
---
+++ src/helpers/getDocumentationLinks.ts
@@ -0,0 +1,21 @@
+const LIGHTHOUSE_DOCUMENTATION_WEBSITE = 'https://web.dev';
+const regexMarkdownLink = /\[([^[]+)\]\((.*)\)/;
+const regexMarkdownLinks = /(?:__|[*#])|\[(.*?)\]\(.*?\)/gm;
+
+export default (text: string): Array<string> => {
+  const linkMatches = text.match(regexMarkdownLinks) || [];
+  const links: Array<string> = [];
+
+  for (const linkMatch of linkMatches) {
+    const linkParts = regexMarkdownLink.exec(linkMatch);
+    if (linkParts) {
+      const [, , link] = linkParts;
+
+      // only look for documentation links
+      if (link.includes(LIGHTHOUSE_DOCUMENTATION_WEBSITE)) {
+        links.push(link);
+      }
+    }
+  }
+  return links;
+};
