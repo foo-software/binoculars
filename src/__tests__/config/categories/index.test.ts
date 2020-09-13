d4aeb42d840d19cd4659abb0f78df9f4f4cd8583
Adam Henson <adamhenson1979@gmail.com>
2020-09-13 02:33:08 -0400

    test: groups and categories snapshots

---
+++ src/__tests__/config/categories/index.test.ts
@@ -0,0 +1,18 @@
+import * as categories from '../../../config/categories';
+
+interface DefinitionCollection {
+  [key: string]: any;
+}
+
+const auditCollection: DefinitionCollection = categories;
+
+describe('categories config', () => {
+  for (const key in auditCollection) {
+    describe(`${key}()`, () => {
+      const audit = auditCollection[key]();
+      it('should match snapshot', () => {
+        expect(audit).toMatchSnapshot();
+      });
+    });
+  }
+});
---
+++ src/__tests__/config/groups/index.test.ts
@@ -0,0 +1,18 @@
+import * as groups from '../../../config/groups';
+
+interface DefinitionCollection {
+  [key: string]: any;
+}
+
+const auditCollection: DefinitionCollection = groups;
+
+describe('groups config', () => {
+  for (const key in auditCollection) {
+    describe(`${key}()`, () => {
+      const audit = auditCollection[key]();
+      it('should match snapshot', () => {
+        expect(audit).toMatchSnapshot();
+      });
+    });
+  }
+});
