5d089ae986e7ec448d3ab1ee5cfbfbbb4c3e72d0
Adam Henson <adamhenson1979@gmail.com>
2020-10-06 16:06:43 -0400

    feat: cleanup

---
+++ src/__tests__/config/audits/lighthouse/index.test.ts
@@ -0,0 +1,15 @@
+import * as audits from '../../../../config/audits/lighthouse';
+import Collection from '../../../../interfaces/Collection';
+
+const auditCollection: Collection = audits;
+
+describe('audits config', () => {
+  for (const key in auditCollection) {
+    describe(`${key}()`, () => {
+      const audit = auditCollection[key]();
+      it('should match snapshot', () => {
+        expect(audit).toMatchSnapshot();
+      });
+    });
+  }
+});
