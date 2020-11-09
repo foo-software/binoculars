4a6db715cb2d78a79494690060c0d197fa855a01
adamhenson <adamhenson1979@gmail.com>
2020-11-08 19:44:12 -0500

    feat: slack

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ A tool to measure web page SEO friendliness. Binoculars extends [Google's Lighth
 - Choice programmatic usage or CLI.
 - Run a single audit or multiple audits at once.
 - Upload reports to S3 via simple configuration.
+- Automatically post results as comments in GitHub via commits or pull requests (see [options](#options)).
+- Automatically post results in Slack (see [options](#options)).
 
 # Meaningful Content
 
--- src/helpers/getLighthouseScoreColor.ts
+++ src/helpers/getLighthouseScoreColor.ts
@@ -1,10 +1,10 @@
-// https://developers.google.com/web/tools/lighthouse/v3/scoring
+// https://web.dev/performance-scoring/
 export default ({ isHex, score }: { isHex: boolean; score: number }) => {
   if (typeof score !== 'number') {
     return !isHex ? 'lightgrey' : '#e0e0e0';
   }
 
-  let scoreColor = !isHex ? 'green' : '#0cce6b';
+  let scoreColor = !isHex ? 'green' : '#58e3be';
 
   // medium range
   if (score < 90) {
@@ -13,7 +13,7 @@ export default ({ isHex, score }: { isHex: boolean; score: number }) => {
 
   // bad
   if (score < 50) {
-    scoreColor = !isHex ? 'red' : '#f74531';
+    scoreColor = !isHex ? 'red' : '#fd4b83';
   }
 
   return scoreColor;
